364a4dff3bd903cdc040b40c22855104cd3e954b
Mushmat <166215455+Mushmat@users.noreply.github.com>
2024-06-09 21:39:48 +0530

    Create height_of_BT.cpp

---
+++ height_of_BT.cpp
@@ -0,0 +1,9 @@
+int height(root){
+    if(root == NULL){
+        return 0;
+    }
+    int left = height(root->left);
+    int right = height(root->right);
+    int ans = max(left,right) + 1;
+    return ans;
+}
